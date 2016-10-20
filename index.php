6ea3b2269cb27b8699033b03546ca879a446714b
Juan Carlos Garcia <gjcarlos8@gmail.com>
2016-10-20 02:57:03 +0200

    Un breve formulario y se pueden insertar personas

--- index.php
+++ index.php
@@ -6,19 +6,39 @@
     </head>
     <body>
         <?php
-           $host        = "host=localhost";
-           $port        = "port=5432";
-           $dbname      = "dbname=recetas";
-           $credentials = "user=recetas password=recetas";
+            $host        = "host=localhost";
+            $port        = "port=5432";
+            $dbname      = "dbname=recetas";
+            $credentials = "user=recetas password=recetas";
 
-           $db = pg_connect( "$host $port $dbname $credentials" );
+            $db = pg_connect( "$host $port $dbname $credentials" );
 
-           if(!$db){
-              echo "Error : No se pudo conectar con la base de datos\n";
-           } else {
-              echo "Se ha conectado con la base de datos\n";
-           }
-           ?>
-        <h1>holiiii</h1>
+            if(!$db){
+                echo "Error : No se pudo conectar con la base de datos\n";
+            } else {
+                echo "Se ha conectado con la base de datos\n";
+            }
+
+
+            if(isset($_POST['nombre']) && isset($_POST['anyo'])) {
+                $nombre = $_POST['nombre'];
+                $anyo = $_POST['anyo'];
+
+                // query using prepared statements
+
+                $result = pg_query($db, "INSERT INTO personas (nombre,anyo) VALUES('$nombre', $anyo);");
+
+                if (!$result) {
+                    echo "Ocurrio un error.";
+                } else {
+                    echo "Se inserto correctamente.";
+                }
+            } ?>
+        <h1>Recetas de cocina</h1><br/>
+        <form action="index.php" method="post">
+            <label>Nombre: </label><input type="text" name="nombre" /></br>
+            <label>Año: </label><input type="text" name="anyo" />
+            <input type="submit" value="Confirmar" />
+        </form>
     </body>
 </html>
